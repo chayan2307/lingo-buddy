3aabad4a84d5c12f7d77b2d30da9ac7c4f3adf95
Ella Herlihy <ellaherlihy@gmail.com>
2022-06-06 15:45:00 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "pages#home"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users do
+  devise_for :users
+  resources :users, only: [:index, :show] do
     resources :bookings
-    resources :user_languages
   end
 end
