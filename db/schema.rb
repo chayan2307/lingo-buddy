1cd543800c15536bb08c5e23d69ca8157b17f649
chayan2307 <chayan.agarwal@yahoo.com>
2022-06-06 17:25:41 +0100

    Added code to create cards for teachers

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_06_140251) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_06_06_145850) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_06_140251) do
     t.string "first_name"
     t.string "last_name"
     t.boolean "teacher", default: true
+    t.string "languages"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
