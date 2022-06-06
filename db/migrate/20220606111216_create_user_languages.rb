14bced49dcbb2518b4b379bac8f2f0c44510d1ce
chayan2307 <chayan.agarwal@yahoo.com>
2022-06-06 12:24:35 +0100

    Created models for user, booking and language

--- db/migrate/20220606111216_create_user_languages.rb
+++ db/migrate/20220606111216_create_user_languages.rb
@@ -1,8 +1,6 @@
 class CreateUserLanguages < ActiveRecord::Migration[7.0]
   def change
     create_table :user_languages do |t|
-      t.integer :language_id
-      t.integer :user_id
       t.references :user, null: false, foreign_key: true
       t.references :language, null: false, foreign_key: true
 
--- db/migrate/20220606112318_create_bookings.rb
+++ db/migrate/20220606112318_create_bookings.rb
@@ -2,7 +2,7 @@ class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
       t.integer :user1_id
-      t.integer :user2_id
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
