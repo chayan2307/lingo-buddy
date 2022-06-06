079a884cdc8ea80086630acd702fe98a99871868
Ella Herlihy <ellaherlihy@gmail.com>
2022-06-06 15:15:39 +0100

    booking controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2022_06_06_114641) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_06_06_131324) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_06_140251) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_06_131324) do
     t.index ["user_id"], name: "index_user_languages_on_user_id"
   end
 
->>>>>>> 84714269c36b4ccaa030ceb405b0a29f17fd368d
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -56,8 +49,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_06_131324) do
     t.string "location"
     t.string "first_name"
     t.string "last_name"
+    t.boolean "teacher", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "users"
+  add_foreign_key "user_languages", "languages"
+  add_foreign_key "user_languages", "users"
 end
