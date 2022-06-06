e85edc3c8665de33ecd24265b74aed84ff65b8e2
Ella Herlihy <ellaherlihy@gmail.com>
2022-06-06 15:59:03 +0100

    fixed seed file

---
+++ db/migrate/20220606145850_add_languages_to_user.rb
@@ -0,0 +1,5 @@
+class AddLanguagesToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :languages, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-
 User.destroy_all
 
 puts 'Creating users..'
-User.create(email: 'deborah@email.com', rate: 10, first_name: 'Deborah', last_name: 'Reis')
-User.create(email: 'chayan@email.com', rate: 15, first_name: 'Chayan', last_name: 'Agarwal')
-User.create(email: 'chun@email.com', rate: 20, first_name: 'Chun', last_name: 'Man')
-User.create(email: 'ella@email.com', rate: 25, first_name: 'Ella', last_name: 'Herlihy')
-
+User.create(email: 'deborah@email.com', rate: 10, first_name: 'Deborah', last_name: 'Reis', password: "123456")
+User.create(email: 'chayan@email.com', rate: 15, first_name: 'Chayan', last_name: 'Agarwal', password: "123456")
+User.create(email: 'chun@email.com', rate: 20, first_name: 'Chun', last_name: 'Man', password: "123456")
+User.create(email: 'ella@email.com', rate: 25, first_name: 'Ella', last_name: 'Herlihy', password: "123456")
 
 puts "Finished!"
