f859c4ddb00263cda9fb77004b4cf9d03534dd68
Ella Herlihy <ellaherlihy@gmail.com>
2022-06-07 16:28:47 +0100

    navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
         lng: user.longitude,
         info_window: render_to_string(partial: "info_window", locals: { user: user }),
         image_url: helpers.asset_url('logo.png')
+      }
     end
   end
 
