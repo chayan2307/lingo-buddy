a590f960ba6311dc6a2b5de1dc837a684880ed20
deborah reis <deborahmreis3@gmail.com>
2022-06-08 12:54:32 +0100

    chatroom

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -8,17 +8,20 @@ export default class extends Controller {
   connect() {
       this.channel = createConsumer().subscriptions.create(
         { channel: "ChatroomChannel", id: this.chatroomIdValue },
-        { received: data => {
-          this.messagesTarget.insertAdjacentHTML("beforeend", data)
-          this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
-        }
-        resetForm(event) {
-          event.target.reset()
-        }
-        }
+        { received: data => this.#insertMessageAndScrollDown(data) }
       )
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
 
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    window.scrollTo(0, this.messagesTarget.scrollHeight)
+    // console.log(this.messagesTarget.scrollHeight)
+  }
+
+  resetForm(event) {
+    event.target.reset()
   }
 
   disconnect() {
