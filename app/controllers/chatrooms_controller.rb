bad63ae70d92a62124c9f2380483954574810b9c
deborah reis <deborahmreis3@gmail.com>
2022-06-08 16:33:08 +0100

    okk

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,4 +3,14 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
+
+
+  def create
+    @chatroom = Chatroom.new(chat_params)
+  end
+
+  def chat_params
+    params.require(:chatroom).permit(:name)
+  end
+
 end
