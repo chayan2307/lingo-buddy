6d106c2f3d6202bd6cb72b770442225b8673df1f
Ella Herlihy <ellaherlihy@gmail.com>
2022-06-15 15:12:01 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,6 +52,7 @@ Language.create(language_name: "Russian", photo_url: "https://wallpaperaccess.co
 
 # STUDENT SEEDS BELOW
 puts "creating users"
+User.create(bio: "Hola! My name is Chanyan and I have been on LingoBuddy for 1 years. I love meeting new people and helping them learn Spanish. I live in north of London, hope to see you guys soon!", languages: "Spanish", location: "Wembley, London", email: 'chayan@email.com', rate: 15, first_name: 'Chayan', last_name: 'Agarwal', password: "123456", teacher: true, photo_url: "https://media.istockphoto.com/photos/portrait-of-young-man-in-yellow-tshirt-at-backyard-picture-id1180616266?b=1&k=20&m=1180616266&s=170667a&w=0&h=cMM4OlorRa8ONFxDBQHmAus9H-LkhJ2TDH2px2Hr3M8=")
 User.create(email: 'deborah@email.com', rate: 10, first_name: 'Deborah', last_name: 'Reis', password: "123456")
 # User.create(email: 'chayan@email.com', rate: 15, first_name: 'Chayan', last_name: 'Agarwal', password: "123456")
 User.create(email: 'chun@email.com', rate: 20, first_name: 'Chun', last_name: 'Man', password: "123456")
@@ -129,5 +130,9 @@ end
 # 4.times do
 #   Review.create!(content: bio, rating: 5, user_id: 33)
 # end
+
+5.times do
+  Review.create!(name: Faker::Name.name, content: reviews.sample, rating: rand(3..5), user_id: User.first.id)
+ end
 
 puts "Finished!"
